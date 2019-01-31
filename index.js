121903112b5b55e62d1759e342758ec3c6d7c696
Matthieu Oger <moger@pixelnest.io>
2019-01-31 19:07:27 +0100

    Move dotenv loading to main index file

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import dotenv from 'dotenv'
 import automateTasks from './lib/lbc/index'
 
+// -------------------------------------------------------------
+// Script.
+// -------------------------------------------------------------
+
+// Load environment.
+dotenv.config()
+
+// Automate!
 automateTasks()
